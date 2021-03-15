417c4c5e81609c77858c08ab45221db37d4bd233
Kim Palao <kimfarhantpalao@gmail.com>
2021-03-15 15:13:51 +0000

    Use a connection string for vue-socket.io instead of a SocketIO object. Update local.env

--- overflow-frontend/src/main.ts
+++ overflow-frontend/src/main.ts
@@ -1,13 +1,11 @@
 import { createApp } from 'vue';
 import VueSocketIO from 'vue-socket.io';
 import App from './App.vue';
-// @ts-ignore
-import SocketIO from 'socket.io-client';
 
 const app = createApp(App);
 app.use(new VueSocketIO({
   debug: true,
   // @ts-ignore
-  connection: SocketIO(`http://localhost:3001`), //options object is Optional
+  connection: `http://localhost:${import.meta.env.VITE_BACKEND_PORT}`
 }));
 app.mount('#app');
