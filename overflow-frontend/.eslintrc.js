5df871ef90939af2170e1343130398ad82fa08c2
Kim Palao <kimfarhantpalao@gmail.com>
2021-03-23 05:14:48 +0000

    Modify eslint settings for overflow-frontend to better support TypeScript

--- overflow-frontend/.eslintrc.js
+++ overflow-frontend/.eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  extends: [
-    'plugin:vue/vue3-recommended',
+  plugins: [
+    '@typescript-eslint',
+    'vue',
   ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript'
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 }
