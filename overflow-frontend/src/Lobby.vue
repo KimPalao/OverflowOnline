0d3850abf016ed69478f6b369a17a0e192a38eda
KadinaruDess <67551399+KadinaruDess@users.noreply.github.com>
2021-03-26 02:55:19 +0800

    Added Basics of UC1-S8_UC1-S9 Functionality
    Added a form that submits the name of the player to be kicked (UC1-S8), and
    Added a button that starts the game (UC1-S9).
    
    Note that this is an untested update, due to technical issues with accessing pages. Reasonable, there is a sever lack of UI here.

--- overflow-frontend/src/Lobby.vue
+++ overflow-frontend/src/Lobby.vue
@@ -1,5 +1,25 @@
 <template>
   <h1>Lobby</h1>
+
+  <!-- UC1-S8: Host kicks player from lobby -->
+  <form @submit.prevent="handleSubmit">
+    <div class="form-group">
+      <label></label>
+      <input
+        type="text"
+        class="form-control"
+        v-model="kickedPlayer"
+        placeholder="Kick which Player?"
+      />
+      <button class="btn">{{ "Create" }}</button>
+    </div>
+  </form>
+  <!-- /UC1-S8 -->
+
+  <!-- UC1-S9: Host starts game successfully -->
+  <button @click="onClick()"> {{ "Start Game" }} </button>
+  <!-- /UC1-S9 -->
+
 </template>
 
 <script lang="ts">
@@ -9,7 +29,35 @@ import { defineComponent } from "vue";
  * This is the Lobby page. This is accessed after JoinLobby or CreateLobby.
  */
 export default defineComponent({
+  data() {
+    return {
+      // This is the name of the player that is being kicked
+      kickedPlayer: "",
+    };
+  },
   name: "Lobby",
+  sockets: {
+    gameStartEvent(){
+      // Navigates to game page
+    },
+    kickEvent() {
+      this.store.state.lobbyCode = "";
+      // navigate to LobbyMenu
+    }
+  },
+  methods: {
+    handleSubmit(){
+      if (this.kickedPlayer.trim().length == 0){
+        alert("Please enter a Username");
+        return;
+      }
+      this.$socket.emit('kickPlayer', this.kickedPlayer);
+    },
+    onClick(){
+      this.$socket.emit('startGame');
+    },
+  },
+
   mounted() {
     console.log("Lobby");
   },
