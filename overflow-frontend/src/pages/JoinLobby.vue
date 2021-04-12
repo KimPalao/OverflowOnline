097221f0586463365c1c21b16fcbc0bb6fd9238a
Joyce <clsandoval@up.edu.ph>
2021-04-12 23:23:39 +0800

    Finish frontend documentation

--- overflow-frontend/src/pages/JoinLobby.vue
+++ overflow-frontend/src/pages/JoinLobby.vue
@@ -56,6 +56,7 @@ export default defineComponent({
     },
   },
   methods: {
+    // Players get an error alert if the lobby code entered is blank
     handleSubmit() {
       if (this.lobbyCode.trim().length == 0) {
         alert("Code cannot be blank");
